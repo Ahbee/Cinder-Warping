538676707942c23f2c8dfbd761900ea67ea46f66
Abhi <Ahbee0@gmail.com>
2014-11-24 22:08:34 -0500

    deleted shit

--- src/Warp.cpp
+++ src/Warp.cpp
@@ -1,22 +1,4 @@
-/*
- Copyright (c) 2010-2013, Paul Houx - All rights reserved.
- This code is intended for use with the Cinder C++ library: http://libcinder.org
-
- This file is part of Cinder-Warping.
-
- Cinder-Warping is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- Cinder-Warping is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with Cinder-Warping.  If not, see <http://www.gnu.org/licenses/>.
-*/
+deleted all your shit
 
 #include "Warp.h"
 #include "WarpBilinear.h"
